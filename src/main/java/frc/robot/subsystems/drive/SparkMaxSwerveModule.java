5d98a19d26cae0c04a9b32446484a06142826646
Jeffrey Tvedt <jeffreytvedt5@gmail.com>
2025-01-11 12:13:24 -0500

    REVLib dependency

---
+++ src/main/java/frc/robot/subsystems/drive/SparkMaxSwerveModule.java
@@ -0,0 +1,24 @@
+package frc.robot.subsystems.drive;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+
+public class SparkMaxSwerveModule implements SwerveModule {
+
+    @Override
+    public void setTargetOpen(Translation2d translation) {
+    }
+
+    @Override
+    public void setTargetOpen(SwerveModuleState state) {
+    }
+
+    @Override
+    public void setTargetClosed(Translation2d translation) {
+    }
+
+    @Override
+    public void setTargetClosed(SwerveModuleState state) {
+    }
+    
+}
