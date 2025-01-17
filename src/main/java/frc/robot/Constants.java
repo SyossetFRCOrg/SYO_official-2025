12a457558af473ba3e85ddf1fa9c54621ca80901
HackerGuy1000 <107278740+HackerGuy1000@users.noreply.github.com>
2025-01-16 19:44:23 -0500

    Continued working on Arm Subsystem

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,32 +5,29 @@
 package frc.robot;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
-  public static class ArmConstants
-  {
-    public static enum ArmAngle
-    {
-      CORAL_STATION,
-      L1,
-      L2_OR_L3,
-      L4,
-    }
-    public static enum ArmState
-    {
-      IDLE,
-      INTAKE,
-      OUTTAKE,
-    }
+
+  public static class ArmConstants {
+    //TODO replace with real values
+    public static final int ARM_INTAKE_PORT = 0;
+    public static final int ARM_WRIST_PORT = 1;
+
+    public static final double ARM_INTAKE_SPEED = 0.5;
+    public static final double ARM_OUTTAKE_SPEED = -0.5;
 
   }
 }
--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -4,11 +4,33 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ArmSubsystem extends SubsystemBase {
+  SparkMax coralWrist;
+  SparkMaxConfig motorConfig;
+
   /** Creates a new ArmSubsystem. */
-  public ArmSubsystem() {}
+  public ArmSubsystem() {
+    coralWrist = new SparkMax(0,MotorType.kBrushless);
+
+    //TODO Go through this and change it, just pasted in for syntax
+    motorConfig = new SparkMaxConfig();
+    motorConfig.inverted(true).idleMode(IdleMode.kBrake);
+    motorConfig.encoder.positionConversionFactor(1000).velocityConversionFactor(1000);
+    motorConfig.closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder).pid(1.0, 0.0, 0.0);
+    coralWrist.configure(motorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    
+
+  }
 
   @Override
   public void periodic() {
