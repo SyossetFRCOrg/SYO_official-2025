ffa18b8018314c1525284c9fb66f84a26badabc2
theMEHMET-1 <mercanmeh1234@gmail.com>
2025-02-01 20:59:18 -0500

    added ff and trapezoidal profile implementation for WristIOSparkMax

--- src/main/java/frc/robot/subsystems/wrist/WristIOSparkMax.java
+++ src/main/java/frc/robot/subsystems/wrist/WristIOSparkMax.java
@@ -9,7 +9,10 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
+
+import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.filter.Debouncer;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.util.Units;
@@ -21,15 +24,17 @@ public class WristIOSparkMax implements WristIO {
   private final RelativeEncoder encoder;
   private final SparkMaxConfig sparkConfig = new SparkMaxConfig();
 
+  private final double GEAR_RATIO = 1;
+
   private final Debouncer connectedDebounce = new Debouncer(0.5);
 
-  // private ArmFeedforward ff;
+  private SimpleMotorFeedforward ff;
   private static final LoggedTunableNumber kP = new LoggedTunableNumber("Wrist/kP", 0);
   private static final LoggedTunableNumber kD = new LoggedTunableNumber("Wrist/kD", 0);
   private static final LoggedTunableNumber maxVelocity =
-      new LoggedTunableNumber("Wrist/MaxVelocity", 0);
+      new LoggedTunableNumber("Wrist/MaxVelocity", 0.3);
   private static final LoggedTunableNumber maxAcceleration =
-      new LoggedTunableNumber("Wrist/MaxAcceleration", 0);
+      new LoggedTunableNumber("Wrist/MaxAcceleration", 0.1);
 
   private ProfiledPIDController pidController =
       new ProfiledPIDController(
@@ -39,6 +44,9 @@ public class WristIOSparkMax implements WristIO {
           new TrapezoidProfile.Constraints(maxVelocity.get(), maxAcceleration.get()));
 
   public WristIOSparkMax() {
+    ff = new SimpleMotorFeedforward(0, 12 / 5600 * GEAR_RATIO, 0);
+
+    
     sparkMax = new SparkMax(-2, MotorType.kBrushless);
     encoder = sparkMax.getEncoder();
 
@@ -46,11 +54,17 @@ public class WristIOSparkMax implements WristIO {
     sparkConfig.idleMode(IdleMode.kBrake);
 
     sparkMax.configure(sparkConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+    pidController.reset(Units.rotationsToRadians(encoder.getPosition()));
+
   }
 
   @Override
   public void runPosition(double position) {
+    pidController.setGoal(position);
+
     sparkMax.setVoltage(
+        ff.calculate(pidController.getSetpoint().velocity) +
         pidController.calculate(Units.radiansToRotations(position), encoder.getPosition()));
   }
 
