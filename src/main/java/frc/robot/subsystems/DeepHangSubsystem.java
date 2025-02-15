a89fa7969eb9b30b6c45b5f889dcbe3baf33412e
HackerGuy1000 <107278740+HackerGuy1000@users.noreply.github.com>
2025-02-15 10:12:38 -0500

    removed deep hang toml config

--- src/main/java/frc/robot/subsystems/DeepHangSubsystem.java
+++ src/main/java/frc/robot/subsystems/DeepHangSubsystem.java
@@ -11,7 +11,6 @@ import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
-import com.moandjiezana.toml.Toml;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
