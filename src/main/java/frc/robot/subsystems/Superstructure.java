dfba2b0e2883530aeeab622d442c551bf00defc9
HackerGuy1000 <107278740+HackerGuy1000@users.noreply.github.com>
2025-02-15 11:03:12 -0500

    changed deep hang controls

--- src/main/java/frc/robot/subsystems/Superstructure.java
+++ src/main/java/frc/robot/subsystems/Superstructure.java
@@ -76,11 +76,11 @@ public class Superstructure extends SubsystemBase {
         if (subsystems.get("deep_hang").getBoolean("enabled")) {
             deepHang = Optional.of(new DeepHangSubsystem());
             deepHang.ifPresent(hang -> {
-                controller.rightTrigger().and(controller.x())
+                controller.leftTrigger().and(controller.x())
                         .whileTrue(Commands.startEnd(() -> {
                             hang.setVoltage(3.0);
                         }, () -> hang.setVoltage(0.0)));
-                controller.rightTrigger().and(controller.y())
+                controller.leftTrigger().and(controller.y())
                         .whileTrue(Commands.startEnd(() -> {
                             hang.setVoltage(-3.0);
                         }, () -> hang.setVoltage(0.0)));
