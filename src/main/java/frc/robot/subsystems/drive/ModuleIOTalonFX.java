661cf2477bfb966d5520329aa42aae79f43ebc42
niero369 <subscribe2niero@gmail.com>
2025-12-11 17:36:31 -0500

    comp robotics fixed CANBus

--- src/main/java/frc/robot/subsystems/drive/ModuleIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/drive/ModuleIOTalonFX.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems.drive;
 import static frc.robot.util.PhoenixUtil.*;
 
 import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.CANBus;
 import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.CANcoderConfiguration;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
@@ -27,7 +28,6 @@ import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Current;
 import edu.wpi.first.units.measure.Voltage;
-import frc.robot.generated.TunerConstants;
 import java.util.Queue;
 
 /**
@@ -42,8 +42,10 @@ public class ModuleIOTalonFX implements ModuleIO {
       constants;
 
   // Hardware objects
+  private final CANBus driveCANBus;
   private final TalonFX driveTalon;
   private final TalonFX turnTalon;
+  private final CANBus cancoderCANbus;
   private final CANcoder cancoder;
 
   // Voltage control requests
@@ -85,9 +87,17 @@ public class ModuleIOTalonFX implements ModuleIO {
       SwerveModuleConstants<TalonFXConfiguration, TalonFXConfiguration, CANcoderConfiguration>
           constants) {
     this.constants = constants;
-    driveTalon = new TalonFX(constants.DriveMotorId, TunerConstants.DrivetrainConstants.CANBusName);
-    turnTalon = new TalonFX(constants.SteerMotorId, TunerConstants.DrivetrainConstants.CANBusName);
-    cancoder = new CANcoder(constants.EncoderId, "*");
+    // driveTalon = new TalonFX(constants.DriveMotorId,
+    // TunerConstants.DrivetrainConstants.CANBusName);
+    // turnTalon = new TalonFX(constants.SteerMotorId,
+    // TunerConstants.DrivetrainConstants.CANBusName);
+
+    driveCANBus = new CANBus(); // default value "rio"
+    driveTalon = new TalonFX(constants.DriveMotorId, driveCANBus);
+    turnTalon = new TalonFX(constants.SteerMotorId, driveCANBus);
+
+    cancoderCANbus = new CANBus("*");
+    cancoder = new CANcoder(constants.EncoderId, cancoderCANbus);
 
     // Configure drive motor
     var driveConfig = constants.DriveMotorInitialConfigs;
